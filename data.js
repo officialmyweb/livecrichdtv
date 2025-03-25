4b1a8fd9d0e59426bf1d83e63b0be44cbd950748
crushxguru <adarshmishra.pj@gmail.com>
2025-03-25 09:56:49 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 const m3u8Links = {
-  m3u81: "https://prod-sports-eng-gm.jiocinema.com/hls/live/2100307/hd_akamai_merged_avc_eng_cricket_m5_241224/master.m3u8",
-  m3u82: "https://prod-sports-eng-gm.jiocinema.com/hls/live/2100307/hd_akamai_merged_avc_eng_cricket_m5_241224/master.m3u8",
+  m3u81: "",
+  m3u82: "",
   // Add more links as needed
 };
