fc06ce7bcbd6fffa20e87aa9cb0d81b98d484e36
crushxguru <adarshmishra.pj@gmail.com>
2025-04-04 17:52:39 +0530

    Create ios.js

---
+++ ios.js
@@ -0,0 +1,33 @@
+if (confirm("Join Our Telegram Channel @livecrichdofficial")) {
+  window.location.href = "https://telegram.me/livecrichdofficial";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const links = [
+    {
+      href: "https://iframv3.embedxt.site/iframe/frame.php",
+      text: "WORLDWIDE/OUT OF INDIA USERS",
+    },
+    {
+      href: "https://shz.al/Sffb/livecricplayer.html?dtv=https://prod-ent-live-gm.jiocinema.com/bpk-tv/JS_M1_IPL25_ENG_IDC_MOB/Fallback/index.m3u8",
+      text: "ENGLISH - INDIAN USERS",
+    },
+    {
+      href: "https://shz.al/Sffb/livecricplayer.html?dtv=https://prod-ent-live-gm.jiocinema.com/bpk-tv/JS_M1_IPL25_HIN_IDC_MOB/Fallback/index.m3u8",
+      text: "HINDI - INDIAN USERS",
+    },
+  ];
+
+  const container = document.getElementById("link-container");
+
+  links.forEach(link => {
+    const p = document.createElement("p");
+    const a = document.createElement("a");
+    a.href = link.href;
+    a.target = "_blank";
+    a.className = "link-box";
+    a.textContent = link.text;
+    p.appendChild(a);
+    container.appendChild(p);
+  });
+});
