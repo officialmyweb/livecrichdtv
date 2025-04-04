da752db36f12dfc777e966813d37e9e8e87c1c66
crushxguru <adarshmishra.pj@gmail.com>
2025-04-04 17:33:56 +0530

    Update streams.js

--- streams.js
+++ streams.js
@@ -1,8 +1,1 @@
-const streams = {
-    "m1": "https://amg01269-amg01269c1-willowtv-us-5479.playouts.now.amagi.tv/playlist/amg01269-willowtvfast-willowplus-willowtvus/playlist.m3u8",
-    "m2": "https://prod-sports-eng-gm.jiocinema.com/hls/live/2100306/hd_akamai_merged_avc_eng_cricket_m2_110225/master.m3u8",
-    "m3": "https://prod-sports-eng-gm.jiocinema.com/hls/live/2100306/hd_akamai_merged_avc_eng_cricket_m1_110225/master.m3u8",
-    "m4": "https://prod-sports-eng-gm.jiocinema.com/hls/live/2100306/hd_akamai_merged_avc_eng_cricket_m3_120225/master.m3u8",
-    "5": "https://example.com/link5.m3u8",
-    "6": "https://example.com/link6.m3u8"
-};
+.
