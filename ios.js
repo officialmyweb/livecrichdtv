620450e7440aab198b33f7a1dc602a93face7d67
crushxguru <adarshmishra.pj@gmail.com>
2025-04-08 19:26:29 +0530

    Update ios.js

--- ios.js
+++ ios.js
@@ -8,14 +8,6 @@ document.addEventListener("DOMContentLoaded", function () {
       href: "https://shz.al/Sffb/livecricplayer.html?dtv=https://v18tataplaysyndication.akamaized.net/bpk-tv/Sports18_1_HD_voot_MOB/output03/index.m3u8?hdnea=exp=1744120677~acl=/*~hmac=f8562ded4835a5fd2f158a925923dab1f2f3d76cdef75e0dfae693051dd5bcab",
       text: "HINDI ALL USERS - S1",
     },
-    {
-      href: "#",
-      text: "HINDI ALL USERS - S2",
-    },
-    {
-      href: "https://shz.al/Sffb/livecricplayer.html?dtv=https://vkvsd250.okcdn.ru/cmaf/8174863256108/sig/BxYlgbYeMHU/srcIp/172.69.178.85/urls/45.136.20.85/expires/1744407523175/clientType/13/srcAg/CHROME/fromCache/1/mid/9467564993324/id/8174863256108/get/hls_9467564993324.M8H9Ktvtoeg.m3u8",
-      text: "FOX CRICKET- ONLY MOBILE USERS",
-    },
   ];
 
   const container = document.getElementById("link-container");
