6794ace85c80419f460d8a532e183313395858b8
crushxguru <adarshmishra.pj@gmail.com>
2025-04-08 19:29:30 +0530

    Update ios.js

--- ios.js
+++ ios.js
@@ -5,7 +5,7 @@ if (confirm("Join Our Telegram Channel @livecrichdofficial")) {
 document.addEventListener("DOMContentLoaded", function () {
   const links = [
     {
-      href: "https://shz.al/Sffb/livecricplayer.html?dtv=https://v18tataplaysyndication.akamaized.net/bpk-tv/Sports18_1_HD_voot_MOB/output03/index.m3u8?hdnea=exp=1744120677~acl=/*~hmac=f8562ded4835a5fd2f158a925923dab1f2f3d76cdef75e0dfae693051dd5bcab",
+      href: "https://shz.al/Sffb/livecricplayer.html?dtv=https://v18tataplaysyndication.akamaized.net/bpk-tv/Sports18_1_HD_voot_MOB/output03/index.m3u8?hdnea=exp=1744134327~acl=/*~hmac=15313c96585eb0347d1895a069afb5af85299c347d4951a385c0f7b3ca5022c8",
       text: "HINDI ALL USERS - S1",
     },
   ];
