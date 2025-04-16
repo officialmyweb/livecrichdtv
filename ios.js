85cba4f7fd05962ad8937b901e828a14a6a5eeee
crushxguru <adarshmishra.pj@gmail.com>
2025-04-16 18:56:57 +0530

    Update ios.js

--- ios.js
+++ ios.js
@@ -6,15 +6,15 @@ document.addEventListener("DOMContentLoaded", function () {
   const links = [
     {
       href: "https://livecrichdtv.pages.dev/ww",
-      text: "HIN + ENG - OUT + IN INDIA",
+      text: "HIN + ENG - WORLDWIDE",
     },
     {
       href: "https://s13.24cwc.com/",
-      text: "ALL DEVICE WORKING - INDIA + OUT OF INDIA ALSO",
+      text: "ALL DEVICE WORKING -WORLDWIDE ",
     },
     {
       href: "https://livecrichdtv.pages.dev/www",
-      text: "OUT OF INDIA USERS - S2",
+      text: "ONLY WORLDWIDE - S2",
     },
   ];
 
