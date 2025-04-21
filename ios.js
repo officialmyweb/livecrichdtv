f2330335f1c4f9f45cb463c9b376a1f72d74d363
crushxguru <adarshmishra.pj@gmail.com>
2025-04-21 17:59:10 +0530

    Update ios.js

--- ios.js
+++ ios.js
@@ -5,10 +5,6 @@ if (confirm("Join Our Telegram Channel @livecrichdofficial")) {
 document.addEventListener("DOMContentLoaded", function () {
   const links = [
     {
-      href: "https://shz.al/Sffb/livecricplayer.html?dtv=https://v18tataplaysyndication.akamaized.net/bpk-tv/StarSports_2_Hin_HD_voot_MOB/output03/index.m3u8?hdnea=exp=1745160156~acl=/*~hmac=8b5d9983fd219cce341ec12fd598b090aaffe83953dd5f4ca5318517bf5b2602",
-      text: "HIN - WORLDWIDE",
-    },
-    {
       href: "https://livecrichdtv.pages.dev/ww",
       text: "HIN + ENG - WORLDWIDE",
     },
