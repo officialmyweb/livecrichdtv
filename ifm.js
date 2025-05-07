d76b76235b132eabdab2bee445c8bca8112d48ac
crushxguru <adarshmishra.pj@gmail.com>
2025-05-08 00:15:32 +0530

    Update ifm.js

--- ifm.js
+++ ifm.js
@@ -4,7 +4,7 @@
 // Define your streams
 const streams = {
     m1: "https://la12hd.com/vivo/canales.php?stream=calientetv",
-    m2: "https://streamtp4.com/global1.php?stream=max1",
+    m2: "https://streamtp4.com/global1.php?stream=paramount1",
     m3: "https://streamtp4.com/global1.php?stream=paramount4",
     m4: "https://envivo1.com/canal.php?stream=paramount2"
 };
