f2b0a5c1d459313a80156bbf147e7d5dc6039fe4
crushxguru <adarshmishra.pj@gmail.com>
2025-05-24 19:33:39 +0530

    Update ifm.js

--- ifm.js
+++ ifm.js
@@ -3,9 +3,9 @@
  }
 // Define your streams
 const streams = {
-    m1: "https://streamtp4.com/global1.php?stream=paramount1",
-    m2: "https://streamtp4.com/global1.php?stream=foxsportspremium",
-    m3: "https://streamtp4.com/global1.php?stream=paramount4",
+    m1: "https://la12hd.com/vivo/canales.php?stream=espnplus2",
+    m2: "https://streamtp4.com/global1.php?stream=espndeportes",
+    m3: "https://p.apeiria.net/~football/cricXfootballplayer.html?dtv=https://dsjz1gms3zce5.cloudfront.net/out/v1/f413830d21d044918adac53dafc1fe3b/index.m3u8",
     m4: "https://envivo1.com/canal.php?stream=paramount2"
 };
 
