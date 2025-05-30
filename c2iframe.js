93964dd1d7f6af739cf9839113f926869c8bc2e3
crushxguru <adarshmishra.pj@gmail.com>
2025-05-30 15:25:26 +0530

    Update c2iframe.js

--- c2iframe.js
+++ c2iframe.js
@@ -3,7 +3,7 @@
  }
 // Define your streams
 const streams = {
-    m1: "https://p.apeiria.net/~player/fcplayer.html?dtv=https%3A%2F%2Famssandbrix.b-cdn.net%2FLiveApp%2Fstreams%2FNKAv2V2BTnEuVJdS170390248731.m3u8",
+    m1: "https://p.apeiria.net/~player/fcplayer.html?dtv=https://bcovlive-a.akamaihd.net/b238876e5f004ec29d4e07066a044996/eu-west-1/4854911276001/playlist.m3u8",
     m2: "https://streamtp4.com/global1.php?stream=espndeportes",
     m3: "https://p.apeiria.net/~football/cricXfootballplayer.html?dtv=https://dsjz1gms3zce5.cloudfront.net/out/v1/f413830d21d044918adac53dafc1fe3b/index.m3u8",
     m4: "https://envivo1.com/canal.php?stream=paramount2"
