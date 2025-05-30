bff08d750dce3bc23928aec463f4e4fc96a10722
crushxguru <adarshmishra.pj@gmail.com>
2025-05-30 15:53:17 +0530

    Update c2iframe.js

--- c2iframe.js
+++ c2iframe.js
@@ -1,9 +1,9 @@
 // Define your streams
 const streams = {
     m1: "https://p.apeiria.net/~player/fcplayer.html?dtv=https://bcovlive-a.akamaihd.net/b238876e5f004ec29d4e07066a044996/eu-west-1/4854911276001/playlist.m3u8",
-    m2: "https://streamtp4.com/global1.php?stream=espndeportes",
-    m3: "https://p.apeiria.net/~football/cricXfootballplayer.html?dtv=https://dsjz1gms3zce5.cloudfront.net/out/v1/f413830d21d044918adac53dafc1fe3b/index.m3u8",
-    m4: "https://envivo1.com/canal.php?stream=paramount2"
+    m2: "#",
+    m3: "#",
+    m4: "#"
 };
 
 // Get the "id" parameter from URL
