73c6617c6d6bebf23eb9a695b22cea76dea870a0
crushxguru <adarshmishra.pj@gmail.com>
2025-06-10 21:42:36 +0530

    Update premium.js

--- premium.js
+++ premium.js
@@ -1,7 +1,7 @@
 const streamMap = {
   "sky": {
     url: "https://mi9hodru2lnlspaswl8regarijequchiy.happy-ending.site/CRujuthiswatothi5hlcata7ephow0xltaCrEp0n2qataj.m3u8",
-    poster: "https://example.com/poster1.jpg"
+    poster: "https://i.ibb.co/MyvXYC4Z/IMG-20250610-214129.jpg"
   },
   "M2": {
     url: "https://example.com/stream2.m3u8",
