8d410fd3ca7866cea92d3065c8c2080ca454db93
crushxguru <adarshmishra.pj@gmail.com>
2025-06-10 21:37:29 +0530

    Update premium.js

--- premium.js
+++ premium.js
@@ -1,6 +1,14 @@
-// streams.js
 const streamMap = {
-  "M1": "https://d3qg0hadt0dkov.cloudfront.net/v1/master/3722c60a815c199d9c0ef36c5b73da68a62b09d1/cc-3sfgyrfz9zduo-prod/amgtennisus/playlist.m3u8",
-  "M2": "https://example.com/stream2.m3u8",
-  "M3": "https://example.com/stream3.m3u8"
+  "sky": {
+    url: "https://mi9hodru2lnlspaswl8regarijequchiy.happy-ending.site/CRujuthiswatothi5hlcata7ephow0xltaCrEp0n2qataj.m3u8",
+    poster: "https://example.com/poster1.jpg"
+  },
+  "M2": {
+    url: "https://example.com/stream2.m3u8",
+    poster: "https://example.com/poster2.jpg"
+  },
+  "M3": {
+    url: "https://example.com/stream3.m3u8",
+    poster: "https://example.com/poster3.jpg"
+  }
 };
