772e73926c91a5e276b26fdf012a759f290cf16d
crushxguru <adarshmishra.pj@gmail.com>
2025-06-10 21:27:43 +0530

    Create premium.js

---
+++ premium.js
@@ -0,0 +1,6 @@
+// streams.js
+const streamMap = {
+  "M1": "https://d3qg0hadt0dkov.cloudfront.net/v1/master/3722c60a815c199d9c0ef36c5b73da68a62b09d1/cc-3sfgyrfz9zduo-prod/amgtennisus/playlist.m3u8",
+  "M2": "https://example.com/stream2.m3u8",
+  "M3": "https://example.com/stream3.m3u8"
+};
