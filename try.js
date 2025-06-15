91f496e5deb95b86a2c76dcc56f35c3eb7492815
crushxguru <adarshmishra.pj@gmail.com>
2025-06-15 23:21:08 +0530

    Create try.js

---
+++ try.js
@@ -0,0 +1,13 @@
+const streams = {
+  m_name: {
+    type: "shaka",
+    url: "https://bpcdncs3.dialog.lk/bpk-token/2ab@zj24gbhur11e2mxdcntvky3ncoocx0ubocquppba/GlobalManifest.mpd",
+    clearKeys: {
+      "2fffb62dde135028a768450907fbc9cb": "c6f9915f1444b12985ca16f56a632b85"
+    }
+  },
+  n_name: {
+    type: "clappr",
+    url: "https://example.com/yourstream.m3u8"
+  }
+};
