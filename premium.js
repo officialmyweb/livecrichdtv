6e07149959531aaaf704ff27020fa5a46397dff2
crushxguru <adarshmishra.pj@gmail.com>
2025-06-15 22:06:41 +0530

    Update premium.js

--- premium.js
+++ premium.js
@@ -4,7 +4,7 @@ const streamMap = {
     poster: "https://i.ibb.co/MyvXYC4Z/IMG-20250610-214129.jpg"
   },
   "f1": {
-    url: "https://dluorh7ry2690.cloudfront.net/out/v1/4f2a5c13dd4e48deabd974bb119c7b78/index.m3u8",
+    url: "https://d2sshbxprtwbjg.cloudfront.net/out/v1/5a00690119224df688a7c3d2e9c7022f/index.m3u8",
     poster: "https://i.ibb.co/0RjkVHK0/IMG-20250614-213759-410.jpg"
   },
   "match2": {
