84df0d4163e915d1d7cb0c78dc512996431a3fdb
crushxguru <adarshmishra.pj@gmail.com>
2025-06-15 23:22:23 +0530

    Update try.js

--- try.js
+++ try.js
@@ -1,13 +1,13 @@
 const streams = {
   m_name: {
     type: "shaka",
-    url: "https://bpcdncs3.dialog.lk/bpk-token/2ab@zj24gbhur11e2mxdcntvky3ncoocx0ubocquppba/GlobalManifest.mpd",
+    url: "https://p7s1-live-009-prod.akamaized.net/27a90190/t_009/sat1-de-hd/cenc-default.mpd",
     clearKeys: {
-      "2fffb62dde135028a768450907fbc9cb": "c6f9915f1444b12985ca16f56a632b85"
+      "3a2ac903e4299a7815111e256369d9d4": "540313f2e5eda2c17df277afc085f33c"
     }
   },
   n_name: {
     type: "clappr",
-    url: "https://example.com/yourstream.m3u8"
+    url: "https://d2sshbxprtwbjg.cloudfront.net/out/v1/5a00690119224df688a7c3d2e9c7022f/index.m3u8"
   }
 };
