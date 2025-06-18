ab60f7d668f7e2dfcb34fd35b9f3516b0e849d76
crushxguru <adarshmishra.pj@gmail.com>
2025-06-19 00:36:31 +0530

    Update ifm.js

--- ifm.js
+++ ifm.js
@@ -11,7 +11,7 @@ if (confirm("Join Our Telegram Channel @cricxfootball")) {
 
 // ✅ Streams list
 const streams = {
-    m1: "https://stream196tp.com/global1.php?stream=dazntv2",
+    m1: "https://envivo5.com/canal.php?stream=tudn_usa",
     m2: "https://stream196tp.com/global1.php?stream=tudn_usa",
     m3: "#",
     m4: "https://streamtp4.com/global1.php?stream=disney10",
