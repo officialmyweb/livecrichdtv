7c1fc9f823e6566291bddf1d5bf491ccdcca305a
crushxguru <adarshmishra.pj@gmail.com>
2025-06-20 20:27:08 +0530

    Update premium.js

--- premium.js
+++ premium.js
@@ -3,9 +3,9 @@ const streamMap = {
     url: "https://nijineh-lalajo.hf.space/proxy?url=https://dokko1new.newkso.ru/dokko1/premium65/mono.m3u8",
     poster: "https://i.ibb.co/MyvXYC4Z/IMG-20250610-214129.jpg"
   },
-  "f1": {
-    url: "https://d2sshbxprtwbjg.cloudfront.net/out/v1/5a00690119224df688a7c3d2e9c7022f/index.m3u8",
-    poster: "https://i.ibb.co/0RjkVHK0/IMG-20250614-213759-410.jpg"
+  "sony3": {
+    url: "https://uugg.hatsune86.space/lchd-sonyten3.m3u8",
+    poster: "#"
   },
   "fox501": {
     url: "https://uugg.hatsune86.space/lchd-foxsports501.m3u8",
