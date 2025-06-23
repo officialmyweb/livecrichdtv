568ef1f67ecfe2a22063d136023508bb2d212c54
crushxguru <adarshmishra.pj@gmail.com>
2025-06-23 15:08:46 +0530

    Update premium.js

--- premium.js
+++ premium.js
@@ -1,6 +1,6 @@
 const streamMap = {
   "sky": {
-    url: "#",
+    url: "https://uugg.hatsune86.space/lchd-skycric.m3u8",
     poster: "https://i.ibb.co/qLW8GXM8/IMG-20250621-141814.jpg"
   },
   "sony3": {
@@ -8,7 +8,7 @@ const streamMap = {
     poster: "#"
   },
   "fox501": {
-    url: "#",
+    url: "https://uugg.hatsune86.space/lchd-foxsports501.m3u8",
     poster: "https://i.ibb.co/dNp10Xf/IMG-20250620-145214.jpg"
   }
 };
