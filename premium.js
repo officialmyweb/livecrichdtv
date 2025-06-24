d2aa607a730a8131e278c44c8ce5fea430bbf6a7
crushxguru <adarshmishra.pj@gmail.com>
2025-06-24 11:07:15 +0530

    Update premium.js

--- premium.js
+++ premium.js
@@ -8,7 +8,7 @@ const streamMap = {
     poster: "#"
   },
   "fox501": {
-    url: "#",
+    url: "https://uugg.hatsune86.space/lchd-foxsports501.m3u8",
     poster: "https://i.ibb.co/dNp10Xf/IMG-20250620-145214.jpg"
   }
 };
