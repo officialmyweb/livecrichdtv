084fc819f32b1ca26a195aaf02566c1bafd4bc1a
crushxguru <adarshmishra.pj@gmail.com>
2025-06-25 17:17:49 +0530

    Update try.js

--- try.js
+++ try.js
@@ -1,13 +1,1 @@
-const streams = {
-  m_name: {
-    type: "shaka",
-    url: "https://p7s1-live-009-prod.akamaized.net/27a90190/t_009/sat1-de-hd/cenc-default.mpd",
-    clearKeys: {
-      "3a2ac903e4299a7815111e256369d9d4": "540313f2e5eda2c17df277afc085f33c"
-    }
-  },
-  n_name: {
-    type: "clappr",
-    url: "https://d2sshbxprtwbjg.cloudfront.net/out/v1/5a00690119224df688a7c3d2e9c7022f/index.m3u8"
-  }
-};
+.
