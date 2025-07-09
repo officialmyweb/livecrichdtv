fa7e7eab84e7381b2dac15ccc88cfd7f9db85d51
crushxguru <adarshmishra.pj@gmail.com>
2025-07-09 10:51:17 +0530

    Update premium.js

--- premium.js
+++ premium.js
@@ -2,7 +2,7 @@
 const streamMap = {
   "sky": {
     url: "https://1rinij81ehes2uswech6drot7durixl2r0.rtgjmfvikmgtgrvkpbg.shop/j5ru6icHofROsespidec64w0s7lgo8ot.m3u8",
-    poster: "https://i.ibb.co/qLW8GXM8/IMG-20250621-141814.jpg"
+    poster: "https://i.ibb.co/5h7wDqnk/IMG-20250709-105025.jpg"
   },
   "sVa": {
     url: "https://bcovlive-a.akamaihd.net/e04ca35ee2ef47a194008d1a33851a28/ap-southeast-2/5833133821001/75def56f7ff647d0a22b9f7b24a33e51/playlist_ssaiM.m3u8",
