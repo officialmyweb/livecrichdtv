fc51d4f881a7e7026cf9676cb3a0e9530f3e8383
crushxguru <adarshmishra.pj@gmail.com>
2025-07-09 09:08:06 +0530

    Update premium.js

--- premium.js
+++ premium.js
@@ -4,9 +4,9 @@ const streamMap = {
     url: "https://1rinij81ehes2uswech6drot7durixl2r0.rtgjmfvikmgtgrvkpbg.shop/j5ru6icHofROsespidec64w0s7lgo8ot.m3u8",
     poster: "https://i.ibb.co/qLW8GXM8/IMG-20250621-141814.jpg"
   },
-  "cXf": {
-    url: "https://1rinij81ehes2uswech6drot7durixl2r0.rtgjmfvikmgtgrvkpbg.shop/6045367c36aaed715e570adf4e4f7d23.m3u8",
-    poster: "https://i.ibb.co/YBZYhJdt/image-2.webp"
+  "sVa": {
+    url: "https://bcovlive-a.akamaihd.net/e04ca35ee2ef47a194008d1a33851a28/ap-southeast-2/5833133821001/75def56f7ff647d0a22b9f7b24a33e51/playlist_ssaiM.m3u8",
+    poster: "#"
   },
   "fox501": {
     url: "#",
