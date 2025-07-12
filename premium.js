bca008adc5e13b5150c12f53227845384a78cf38
crushxguru <adarshmishra.pj@gmail.com>
2025-07-12 15:19:41 +0530

    Update premium.js

--- premium.js
+++ premium.js
@@ -4,8 +4,8 @@ const streamMap = {
     url: "https://1rinij81ehes2uswech6drot7durixl2r0.rtgjmfvikmgtgrvkpbg.shop/j5ru6icHofROsespidec64w0s7lgo8ot.m3u8",
     poster: "https://i.ibb.co/5h7wDqnk/IMG-20250709-105025.jpg"
   },
-  "sVa": {
-    url: "https://bcovlive-a.akamaihd.net/e04ca35ee2ef47a194008d1a33851a28/ap-southeast-2/5833133821001/75def56f7ff647d0a22b9f7b24a33e51/playlist_ssaiM.m3u8",
+  "tencric": {
+    url: "https://edge3-moblive.yuppcdn.net/drm/smil:tencricketdrm.smil/master.m3u8",
     poster: "#"
   },
   "fox501": {
