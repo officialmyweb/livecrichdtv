c9129ad4da6b70244b41df5e471f14d649a8a1da
crushxguru <adarshmishra.pj@gmail.com>
2025-07-19 22:02:06 +0530

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,26 @@
+const streamMap = {
+  "sky": {
+    url: "https://dai.fancode.com/primary/129720_hindi_hls_18848adfreeta-di_h264/index.m3u8",
+    poster: "https://i.ibb.co/qLW8GXM8/IMG-20250621-141814.jpg",
+    telegramHandle: "@livecrichdofficial",
+    popupMessage: "Join Our Telegram Channel @livecrichdofficial"
+  },
+  "ECBL": {
+    url: "https://dai.fancode.com/primary/129720_hindi_hls_18848adfreeta-di_h264/index.m3u8",
+    poster: "#",
+    telegramHandle: "@cricxfootball",
+    popupMessage: "Join Our Telegram Channel @cricXfootball"
+  },
+  "fox501": {
+    url: "#",
+    poster: "https://i.ibb.co/dNp10Xf/IMG-20250620-145214.jpg",
+    telegramHandle: "@FoxCricketHQ",
+    popupMessage: "Live on Fox Cricket! Join @FoxCricketHQ on Telegram."
+  },
+  "willow": {
+    url: "https://example.com/willow.m3u8",
+    poster: "https://i.ibb.co/V0ZxtGjH/IMG-20250706-160925.jpg",
+    telegramHandle: "@WillowCricketTV",
+    popupMessage: "Watch Willow TV Live – Join our Telegram @WillowCricketTV!"
+  }
+};
