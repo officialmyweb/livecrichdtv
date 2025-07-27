0a64b74c0359239a1e2d0eec89754ebe9c6f541d
crushxguru <adarshmishra.pj@gmail.com>
2025-07-27 15:37:42 +0530

    Update premium.js

--- premium.js
+++ premium.js
@@ -13,7 +13,7 @@ const streamMap = {
     poster: "https://i.ibb.co/dNp10Xf/IMG-20250620-145214.jpg"
   },
   "CLUB": {
-    url: "https://rgelive.akamaized.net/hls/live/2043151/radiolive/playlist.m3u8",
+    url: "https://pull.niues.live/live/stream-414317_lhd.m3u8?auth_key=1753613524-0-0-a9c32e114083b7b36b6e77ba4b73dbaa",
     poster: ""
   },
 
