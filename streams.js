1b76e2f40bde4313a8386dcb8ec61a10ace3aed0
crushxguru <adarshmishra.pj@gmail.com>
2025-08-25 23:09:22 +0530

    Create streams.js

---
+++ streams.js
@@ -0,0 +1,15 @@
+const streams = {
+  "M1": "https://aboutgame.cx/adfree/?id=1",
+  "M2": "https://aboutgame.cx/adfree/?id=2",
+  "M3": "https://aboutgame.cx/adfree/?id=3",
+  "M4": "https://aboutgame.cx/adfree/?id=4",
+  "M1": "https://aboutgame.cx/adfree/?id=1",
+  "M2": "https://aboutgame.cx/adfree/?id=2",
+  "M3": "https://aboutgame.cx/adfree/?id=3",
+  "M4": "https://aboutgame.cx/adfree/?id=4",
+  "M1": "https://aboutgame.cx/adfree/?id=1",
+  "M2": "https://aboutgame.cx/adfree/?id=2",
+  "M3": "https://aboutgame.cx/adfree/?id=3",
+  "M4": "https://aboutgame.cx/adfree/?id=4"
+  
+};
