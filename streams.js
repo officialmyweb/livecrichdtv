31d6083a16081c621f1b6d35c35285cfef2c6a94
crushxguru <adarshmishra.pj@gmail.com>
2025-09-20 13:14:00 +0530

    Update streams.js

--- streams.js
+++ streams.js
@@ -1,16 +1,1 @@
-// streams.js
-const streams = {
-  "M1": {
-    mpd: "https://ev-fast-mpd.starzplayarabia.com/live/eds/CricLifeMax/DASH/CricLifeMax.mpd",
-    keys: {
-      "ad59b720bd4b44d063686212b99484b5": "e1744990edf98acc729c0773a3766583"
-    }
-  },
-  "M2": {
-    mpd: "https://example.com/another-channel.mpd",
-    keys: {
-      "11111111111111111111111111111111": "22222222222222222222222222222222"
-    }
-  }
-  // ➕ Add more channels here
-};
+.
