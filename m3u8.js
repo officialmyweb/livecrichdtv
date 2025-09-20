a58088aaad863c00cc8731ccd18bf89f88bd104b
crushxguru <adarshmishra.pj@gmail.com>
2025-09-20 13:12:03 +0530

    Update m3u8.js

--- m3u8.js
+++ m3u8.js
@@ -1,8 +1,1 @@
-// streams.js
-const streams = {
-  "M1": "https://dai.fancode.com/primary/136742_english_hls_cd7ccd163687982adfreeta-di_h264/index.m3u8",
-  "M2": "https://dai.fancode.com/primary/136742_hindi_hls_03dea3461951169adfreeta-di_h264/index.m3u8",
-  "M3": "https://example.com/stream3/index.m3u8",
-  "M4": "https://example.com/stream4/index.m3u8",
-  "M5": "https://example.com/stream5/index.m3u8"
-};
+.
