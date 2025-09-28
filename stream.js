858777916514d31213dab052c29ecf2c9c6cfdf0
crushxguru <adarshmishra.pj@gmail.com>
2025-09-28 14:45:40 +0530

    Create stream.js

---
+++ stream.js
@@ -0,0 +1,13 @@
+// streams.js
+// Add all channels here with MPD and ClearKey
+const STREAMS = {
+  C1: {
+    mpd: "https://jiotvbpkmob.cdn.jio.com/bpk-tv/Asia_Cup_Hindi_MOB/WDVLive/index.mpd",
+    key: { "31b13c6f241055aaa965263f3115e684":"61f0cbc9599925a7be93d45e30b8ebe9" }
+  },
+  C2: {
+    mpd: "https://example.com/channel2/index.mpd",
+    key: { "abcdef1234567890":"1234567890abcdef" }
+  },
+  // Add more channels here
+};
