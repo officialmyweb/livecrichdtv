381b62ae3a844bb17d329029468ca8be92499e21
crushxguru <adarshmishra.pj@gmail.com>
2025-09-28 14:56:26 +0530

    Update stream.js

--- stream.js
+++ stream.js
@@ -1,13 +1,21 @@
 // streams.js
 // Add all channels here with MPD and ClearKey
 const STREAMS = {
-  C1: {
+  H1: {
     mpd: "https://jiotvbpkmob.cdn.jio.com/bpk-tv/Asia_Cup_Hindi_MOB/WDVLive/index.mpd",
     key: { "31b13c6f241055aaa965263f3115e684":"61f0cbc9599925a7be93d45e30b8ebe9" }
   },
-  C2: {
-    mpd: "https://example.com/channel2/index.mpd",
-    key: { "abcdef1234567890":"1234567890abcdef" }
+  E1: {
+    mpd: "https://jiotvmblive.cdn.jio.com/bpk-tv/Asia_Cup_English_MOB/WDVLive/index.mpd",
+    key: { "978bf56658595ff5a65b960a753beaab":"59af29130ecb16505dec2b00b84c817b" }
+  },
+  T1: {
+    mpd: "https://jiotvmblive.cdn.jio.com/bpk-tv/Asia_Cup_Tamil_MOB/WDVLive/index.mpd",
+    key: { "019329bacb682ae52687226a4d2e8730":"9aaf70c2635657aba43a7517bbbaef7f" }
+  },
+  T2: {
+    mpd: "https://jiotvmblive.cdn.jio.com/bpk-tv/Asia_Cup_Telugu_MOB/WDVLive/index.mpd",
+    key: { "b0018aea46a003d5ddc95487b875a336":"b4efe4dcb98058a2a19d756a4a1f7402" }
   },
   // Add more channels here
 };
