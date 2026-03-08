bce9684e0be0c4d2285b0297d9176d68adf1cfa9
crushxguru <adarshmishra.pj@gmail.com>
2026-03-08 10:50:07 +0530

    Create sweety.js

---
+++ sweety.js
@@ -0,0 +1,45 @@
+const STREAMS = {
+
+H1:{
+name:"SS1",
+mpd:"https://jiotvpllive.cdn.jio.com/bpk-tv/Star_Sports_HD1_Hindi_BTS/output/index.mpd",
+keyId:"400131994b445d8c8817202248760fda",
+key:"2d56cb6f07a75b9aff165d534ae2bfc4"
+},
+
+E1:{
+name:"Channel 2",
+mpd:"https://jiotvpllive.cdn.jio.com/bpk-tv/Star_Sports_HD1_BTS/output/index.mpd",
+keyId:"965dc2ddb1d85138ad787999a7f30ca5",
+key:"859695076e67fe961836b564db6d689c"
+},
+
+M3:{
+name:"Channel 3",
+mpd:"MPD_LINK_HERE",
+keyId:"KEYID_HERE",
+key:"KEY_HERE"
+},
+  
+  M4:{
+name:"Channel 3",
+mpd:"MPD_LINK_HERE",
+keyId:"KEYID_HERE",
+key:"KEY_HERE"
+  },
+
+  M5:{
+name:"Channel 3",
+mpd:"MPD_LINK_HERE",
+keyId:"KEYID_HERE",
+key:"KEY_HERE"
+  },
+
+  M6:{
+name:"Channel 3",
+mpd:"MPD_LINK_HERE",
+keyId:"KEYID_HERE",
+key:"KEY_HERE"
+  }
+
+};
